82cbaeb88cd7007dc0e4d200766854a8505a65d2
fengcaian <caianfeng@yeah.net>
2021-07-15 21:29:33 +0800

    add minus and divide functions, readme.md file

---
+++ README.md
@@ -0,0 +1,21 @@
+smallest module to solve the problem of JS number calculation accuracy
+# To Solve
+```javascript
+0.1 + 0.2 = 0.30000000000000004
+9 * 0.3265 = 2.9385000000000003
+1.21 / 1.1 = 1.0999999999999999
+1 - 0.9 = 0.09999999999999998
+```
+# Install
+```javascript
+npm i number-accurate --save
+```
+# Usage
+```javascript
+import { plusAccurate, timesAccurate, devideAccurate, minusAccurate } from 'number-accurate';
+
+console.log(plusAccurate(0.1, 0.2)); // output 0.3
+console.log(timesAccurate(9, 0.3265)); // output 2.9385
+console.log(devideAccurate(1.21, 1.1)); // output 1.1
+console.log(minusAccurate(1, 0.9)); // output 0.1
+```
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function multiAccurate(arg1, arg2) {
+function timesAccurate(arg1, arg2) {
 	let m = 0;
 	const s1 = arg1.toString();
 	const s2 = arg2.toString();
@@ -18,7 +18,7 @@ function multiAccurate(arg1, arg2) {
 	}
 	return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / Math.pow(10, m);
 }
-function addAccurate(...arg) {
+function plusAccurate(...arg) {
 	let decimalX = 0;
 	let decimalY = 0;
     let result = arg.shift();
@@ -37,9 +37,25 @@ function addAccurate(...arg) {
 		 	decimalY = 0;
 	  	}
 	  	m = Math.pow(10, Math.max(decimalX, decimalY));
-	  	result = (multiAccurate(result, m) + multiAccurate(waitCalNum, m)) / m;
+	  	result = (timesAccurate(result, m) + timesAccurate(waitCalNum, m)) / m;
   	}
  return result;
 }
+function devideAccurate(arg1, arg2) {
+	const reg = /(^([-])?[1-9]\d*(\.\d+)?$)|(^([-])?0(\.\d+)?$)/;
+	if (!reg.test(arg1) || !reg.test(arg2)) {
+		throw new Error('arguments only be number');
+		return;
+	}
+	return timesAccurate(arg1, 1/arg2);
+}
+function minusAccurate(arg1, arg2) {
+	const reg = /(^([-])?[1-9]\d*(\.\d+)?$)|(^([-])?0(\.\d+)?$)/;
+	if (!reg.test(arg1) || !reg.test(arg2)) {
+		throw new Error('arguments only be number');
+		return;
+	}
+	return plusAccurate(arg1, -arg2);
+}
 
-module.exports = { multiAccurate, addAccurate };
+module.exports = { timesAccurate, plusAccurate, devideAccurate, minusAccurate };
